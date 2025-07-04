56f71145ce1ff4efad4baa0518e67a411354b710
Jhonatan Morais <jhonatanvinicius@gmail.com>
2025-07-04 18:10:32 -0300

    chore: init project

---
+++ pre_loader.lua
@@ -0,0 +1,7 @@
+screen_width = 128
+screen_height = 128
+char_size = 4
+cell_size = 8
+max_cells = screen_width/cell_size
+
+_init = scene_main_init
---
+++ scene_main.lua
@@ -0,0 +1,12 @@
+function scene_main_init()
+    _update = scene_main_update
+    _draw = scene_main_draw
+end
+
+function scene_main_update()
+end
+
+function scene_main_draw()
+    cls()
+    ? "main scene"
+end
