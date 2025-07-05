e899f5cdfa63f12426ba134b774f06414751c67e
Jhonatan Morais <jhonatanvinicius@gmail.com>
2025-07-05 15:24:47 -0300

    feat: add pad frame bg

--- pad_frame.lua
+++ pad_frame.lua
@@ -1,7 +1,8 @@
 pad_frame_padding = 2
 pad_frame_w = 10
 pad_frame = {
-    x = 64 - pad_frame_w/2 ,y = 12,w = pad_frame_w,h=70}
+    x = 64 - pad_frame_w/2 ,y = 12,w = pad_frame_w,h=70
+}
 
 function pad_frame_top_bottom_collision(next_y)
 
@@ -20,5 +21,6 @@ function pad_frame_top_bottom_collision(next_y)
 end
 
 function draw_pad_frame()
-    rect( pad_frame.x,pad_frame.y,pad_frame.x + pad_frame.w, pad_frame.y + pad_frame.h,4)
+    rectfill( pad_frame.x,pad_frame.y,pad_frame.x + pad_frame.w, pad_frame.y + pad_frame.h,6)
+    rect( pad_frame.x,pad_frame.y,pad_frame.x + pad_frame.w, pad_frame.y + pad_frame.h,5)
 end
