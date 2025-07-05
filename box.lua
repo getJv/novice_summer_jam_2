f61134f49991a7c91ba13a8f03b0e6a56f86d718
Jhonatan Morais <jhonatanvinicius@gmail.com>
2025-07-05 11:22:09 -0300

    feat: add box sprite

--- box.lua
+++ box.lua
@@ -43,11 +43,7 @@ end
 function draw_box()
     half_w = box.w/2
     half_y = box.h/2
-    rectfill(
-            box.x - half_w,
-            box.y - half_y,
-            box.x + half_w - 1,
-            box.y + half_y - 1,
-            11
-    )
+    spr(1, box.x - half_w, box.y - half_y, 2, 2)
+
+
 end
