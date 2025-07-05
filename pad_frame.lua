7b4ebe512d14107a49141b2d931cbfc373e1acde
Jhonatan Morais <jhonatanvinicius@gmail.com>
2025-07-05 07:05:21 -0300

    feat: add player pad control

--- pad_frame.lua
+++ pad_frame.lua
@@ -6,12 +6,46 @@ pad = {
     w = (pad_frame.x + pad_margin) + 6, -- 6 is the max width
     h = 10
 }
-
-
+pad_down_force = 5
+pad_up_force = 0
 function update_pad_frame()
-    if(can_update(2)) then
-        pad.y = pad.y + 1
+
+    local next_y = pad.y
+
+    -- player action
+    if (btn(4)) then
+        pad_up_force = pad_down_force
+    else
+        pad_up_force = 0
     end
+
+    -- apply gravity
+    if(can_update(4)) then
+        next_y = next_y + pad_down_force
+    end
+
+
+    if(pad_frame_top_bottom_collision(next_y)) then
+        return
+    end
+
+    pad.y = next_y - pad_up_force
+end
+
+function pad_frame_top_bottom_collision(next_y)
+
+    local pad_bottom_y = next_y + pad.h
+    local pad_frame_bottom_y = pad_frame.y + pad_frame.h
+    local hit_pad_frame_bottom = pad_bottom_y > pad_frame_bottom_y
+
+    local pad_top_y = next_y - pad_margin *2
+    local hit_pad_frame_top = pad_top_y < pad_frame.y
+
+    if( hit_pad_frame_bottom or hit_pad_frame_top ) then
+        return true
+    end
+    return false
+
 end
 
 function draw_pad_frame()
